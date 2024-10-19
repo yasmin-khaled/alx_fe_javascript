411311d7dad8cdf97cc06351f20041b40801bdb0
Yasmin Khaled <yasmin.khaled.f@gmail.com>
2024-10-19 19:46:20 +0300

    pushing first task files

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,9 +1,34 @@
-let quotes = [{text:"", category:""}];
-
-function showRandomQuote(){
-
-}
-
-function createAddQuoteForm(){
+document.addEventListener("DOMContentLoaded", ()=>{
+    let quotes = [
+        {
+          text: "A B C.",
+          category: "Motivation"
+        },
+        {
+          text: "AA BB CC.",
+          category: "Inspiration"
+        },
+        {
+          text: "AAA BBB CCC.",
+          category: "Life"
+        },
+        {
+          text: "AAAA BBBB CCCC.",
+          category: "Success"
+        },
+        {
+          text: "AAAAA BBBBB CCCCC.",
+          category: "Motivation"
+        }
+      ];
     
-}
+    function showRandomQuote(){
+        const randomNumber = Math.floor(Math.random() * 5) + 1;
+        const p = `<p>${quotes[randomNumber].text}</p>`;
+        document.body.innerHTML += p;
+    }
+    
+    function createAddQuoteForm(){
+        
+    }
+});
